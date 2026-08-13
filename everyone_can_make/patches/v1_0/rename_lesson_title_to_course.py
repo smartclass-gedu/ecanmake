3e65058eec4da6027c6a155f982885b469f5f0fa
SmartClassroom <hello@gedu.demo.marssys.com>
2026-08-13 04:13:49 +0400

    fix: Use correct Frappe API for checking column existence in migration patch
    - Changed frappe.db.table_has_column() to frappe.db.has_column()
    - Fixes AttributeError during rename_lesson_title_to_course patch execution
    - Allows migration to proceed on Frappe Cloud

--- everyone_can_make/patches/v1_0/rename_lesson_title_to_course.py
+++ everyone_can_make/patches/v1_0/rename_lesson_title_to_course.py
@@ -7,7 +7,7 @@ from frappe.model.utils.rename_field import rename_field
 
 def execute():
 	"""Rename lesson_title field to course on Atomic Learning doctype"""
-	if not frappe.db.table_has_column("Atomic Learning", "lesson_title"):
+	if not frappe.db.has_column("Atomic Learning", "lesson_title"):
 		return
 	frappe.reload_doc("everyone_can_make", "doctype", "atomic_learning")
 	rename_field("Atomic Learning", "lesson_title", "course")
